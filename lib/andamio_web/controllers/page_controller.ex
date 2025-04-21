a8e20860e80ded941afef6af956b43e8ebbadaa0
Julian Vargas <julianvargasalvarez@gmail.com>
2025-04-21 19:10:36 +0200

    Adds trace bullet

--- lib/andamio_web/controllers/page_controller.ex
+++ lib/andamio_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule AndamioWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    IO.puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     render(conn, :home, layout: false)
   end
 end
