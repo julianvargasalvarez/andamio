aaba644baf49e405027b50031cf97afd11b8ef9b
Julian Vargas <julianvargasalvarez@gmail.com>
2025-04-21 13:39:46 +0200

    Firefox attempt

--- config/test.exs
+++ config/test.exs
@@ -38,7 +38,43 @@ config :phoenix_live_view,
 
 config :wallaby, otp_app: :andamio
 
-config :andamio, AndamioWeb.Endpoint,
-  server: true
-
-config :wallaby, :chromedriver, binary: "/usr/local/bin/chromedriver"
+config :wallaby,
+  driver: Wallaby.Selenium,
+  selenium: [
+    remote_url: "http://localhost:4444/",
+    capabilities: %{
+      acceptInsecureCerts: false,
+      browserName: "firefox",
+      browserVersion: "137.0.1",
+      "container:hostname": "6df7f1139bb9",
+      "moz:accessibilityChecks": false,
+      "moz:buildID": "20250407111027",
+      "moz:geckodriverVersion": "0.36.0",
+      "moz:headless": false,
+      "moz:platformVersion": "5.15.167.4-microsoft-standard-WSL2",
+      "moz:processID": 162,
+      "moz:profile": "\u002ftmp\u002frust_mozprofileIVfSHC",
+      "moz:shutdownTimeout": 60000,
+      "moz:webdriverClick": true,
+      "moz:windowless": false,
+      pageLoadStrategy: "normal",
+      platformName: "linux",
+      proxy: %{},
+      "se:bidiEnabled": false,
+      "se:cdpEnabled": false,
+      "se:containerName": "6df7f1139bb9",
+      "se:noVncPort": 7900,
+      "se:vnc": "ws:\u002f\u002f172.17.0.2:4444\u002fsession\u002fc202844a-5d4a-4646-8876-b523e0b19bc8\u002fse\u002fvnc",
+      "se:vncEnabled": true,
+      "se:vncLocalAddress": "ws:\u002f\u002f172.17.0.2:7900",
+      setWindowRect: true,
+      strictFileInteractability: false,
+      timeouts: %{
+        implicit: 0,
+        pageLoad: 300000,
+        script: 30000
+      },
+      unhandledPromptBehavior: "dismiss and notify",
+      userAgent: "Mozilla\u002f5.0 (X11; Linux x86_64; rv:137.0) Gecko\u002f20100101 Firefox\u002f137.0"
+    }
+  ]
