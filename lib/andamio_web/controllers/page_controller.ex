e775288e548c2173f6b1a9da5dcf5c45ea20feef
Julian Vargas <julianvargasalvarez@gmail.com>
2025-04-30 22:45:15 +0200

    Tests passing and added .tool-versions file

--- lib/andamio_web/controllers/page_controller.ex
+++ lib/andamio_web/controllers/page_controller.ex
@@ -4,7 +4,6 @@ defmodule AndamioWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    IO.puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     render(conn, :home, layout: false)
   end
 end
