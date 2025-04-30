6a9db16b554d7a0b4c7b9ee45b228798f7ac2b16
Julian Vargas <julianvargasalvarez@gmail.com>
2025-04-30 23:11:36 +0200

    Add first test for accually creating a startup

--- test/features/creating_a_big_company_test.exs
+++ test/features/creating_a_big_company_test.exs
@@ -1,10 +1,14 @@
 defmodule AndamioWeb.CreateBigCompanyTest do
   use ExUnit.Case, async: true
   use Wallaby.Feature
+  import Wallaby.Browser
 
   feature "User creates a Big Company", %{session: session} do
     session
     |> visit("/")
     |> assert_has(Query.css("body", text: "These are your Big Companies"))
+    |> fill_in(Query.text_field("Name"), with: "My startup")
+    |> click(Query.button("Submit"))
+    |> assert_has(Query.text("My startup was created"))
   end
 end
